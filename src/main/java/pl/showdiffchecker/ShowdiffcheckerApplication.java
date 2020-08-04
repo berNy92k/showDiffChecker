f1b56d2bb1d9e199869643407b10766f2651726b
Dawid Bernacki <daw.bernacki@gmail.com>
2020-08-04 22:28:11 +0200

    init commit

---
+++ src/main/java/pl/showdiffchecker/ShowdiffcheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.showdiffchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdiffcheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowdiffcheckerApplication.class, args);
+    }
+
+}
